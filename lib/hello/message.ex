0ab0ce831a3ded3b7bf9c1d0a6c79275ffa9de0a
Anthony Alberti <aalberti333@gmail.com>
2018-12-20 15:18:57 -0500

    first commit

---
+++ lib/hello/message.ex
@@ -0,0 +1,23 @@
+defmodule Hello.Message do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+
+  schema "messages" do
+    field :message, :string
+    field :name, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(message, attrs) do
+    message
+    |> cast(attrs, [:name, :message])
+    |> validate_required([:name, :message])
+  end
+
+  def get_messages(limit \\ 20) do
+    Hello.Repo.all(Hello.Message, limit: limit)
+  end
+end
